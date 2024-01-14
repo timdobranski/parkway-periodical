507199697a4fef62201df915b20594241c0440c9
Tim <timdobranski@gmail.com>
2024-01-13 20:25:06 -0800

    Added tinted photos for intro animation and rendered them in a grid

--- app/public/home/page.js
+++ app/public/home/page.js
@@ -1,10 +1,39 @@
+'use client'
 
+import { useEffect } from 'react';
+const imagePaths = Array.from({ length: 18 }, (v, i) => `/images/intro/${i + 1}.png`);
+import styles from './home.module.css';
 
 export default function Home() {
+  useEffect(() => {
+    const collage = document.getElementById('collage');
+    let scrollAmount = 0;
+
+    const scrollInterval = setInterval(function(){
+      collage.scrollLeft += 1; // Adjust the increment value as needed
+      scrollAmount += 1;
+
+      if(scrollAmount >= collage.scrollWidth - window.innerWidth){
+        clearInterval(scrollInterval);
+      }
+    }, 10); // Adjust the interval as needed
+
+    // Cleanup on component unmount
+    return () => clearInterval(scrollInterval);
+  }, []);
 
   return (
     <div className='pageWrapper'>
-      <h1>Render Posts Here</h1>
+      <div id="collage" className={styles.collage}>
+        {imagePaths.map((path, index) => {
+          const tintClassName = index % 2 === 0 ? 'blueTint' : 'redTint';
+          const combinedClassName = `${styles.collagePhoto} ${styles[tintClassName]}`;
+
+          return (
+            <img key={index} src={path} alt={`Intro Image ${index + 1}`} className={combinedClassName} />
+          );
+        })}
+      </div>
     </div>
   )
 }
