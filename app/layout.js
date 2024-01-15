1766ace5b58e47e33d5d170960800a6bc4dbd4f5
Tim <timdobranski@gmail.com>
2024-01-14 17:19:16 -0800

    Added smooth fade in from black and delayed image render until after load for intro page

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <div className='appContainer'>
+        <div className='black'>
           {children}
         </div>
       </body>
--- app/page.js
+++ app/page.js
@@ -7,9 +7,17 @@ import { useRouter } from 'next/navigation';
 export default function Home() {
   const [rows, setRows] = useState([[], [], []]);
   const baseImagePath = "/images/intro/";
-  const rowsDirectories = ['row1', 'row2', 'row3']; // Directories for each row
-  const imagesPerRow = 10; // Assuming 6 unique images per row
+  const rowsDirectories = ['row1', 'row2', 'row3'];
+  const imagesPerRow = 10;
   const router = useRouter();
+  const [loadedImages, setLoadedImages] = useState(0);
+  const [allImagesLoaded, setAllImagesLoaded] = useState(false);
+
+  const totalImages = 60;
+
+  const handleImageLoad = () => {
+    setLoadedImages(prev => prev + 1);
+  };
 
   useEffect(() => {
     const loadRowImages = (rowDir) => {
@@ -25,23 +33,40 @@ export default function Home() {
     setRows(allRows);
   }, []);
 
-  if (rows[0].length !== 20) return null;
+
+  useEffect(() => {
+    if (loadedImages === totalImages) {
+      setAllImagesLoaded(true);
+    }
+  }, [loadedImages]);
+
+  useEffect(() => {
+    if (allImagesLoaded) {
+      console.log('all images loaded');
+    }
+  }, [allImagesLoaded]);
 
   return (
-    <div className={styles.introWrapper}>
+  <>
       <div className={styles.introContainer}>
-        <img src='images/logos/titledLogo.png' alt='Intro Image' className={styles.introImage} />
+        <img src='images/logos/titledLogoThumb.png' alt='Intro Image' className={styles.introImage} />
         <h1 className={styles.enter} onClick={() => router.push('/public/home')}>ENTER</h1>
       </div>
+      <div className={`${styles.introWrapper} ${allImagesLoaded ? styles.fadeIn : ''}`}>
       {rows.map((rowImages, rowIndex) => (
         <div key={rowIndex} className={styles.imageRow}>
           {rowImages.map((src, index) => (
-            <img key={`${rowIndex}-${index}`} src={src} alt={`Image in row ${rowIndex + 1}, number ${index + 1}`} className={styles.image} />
+            <img
+            key={`${rowIndex}-${index}`}
+            src={src}
+            alt={`Image in row ${rowIndex + 1}, number ${index + 1}`}
+            className={`${styles.image} ${allImagesLoaded ? "" :'hiddenImage'}`}
+            onLoad={handleImageLoad}/>
           ))}
         </div>
       ))}
     </div>
+    </>
   );
-
 }
 
