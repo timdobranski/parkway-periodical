2ddc655f909f93cdd75ec29358c9c7fceafb8c15
Tim <timdobranski@gmail.com>
2024-01-16 09:53:23 -0800

    Updated loadedImages useEffect to display images once state >= total images

--- app/page.js
+++ app/page.js
@@ -64,7 +64,7 @@ export default function Home() {
 
   useEffect(() => {
     console.log('loaded images: ', loadedImages)
-    if (loadedImages === totalImages) {
+    if (loadedImages >= totalImages) {
       setAllImagesLoaded(true);
     }
   }, [loadedImages]);
