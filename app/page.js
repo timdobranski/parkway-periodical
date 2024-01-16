acb3773d676bbe0a412ee87c814ffe5bcd57e51f
Tim <timdobranski@gmail.com>
2024-01-16 09:50:36 -0800

    Updated styling in intro page

--- app/page.js
+++ app/page.js
@@ -63,6 +63,7 @@ export default function Home() {
 
 
   useEffect(() => {
+    console.log('loaded images: ', loadedImages)
     if (loadedImages === totalImages) {
       setAllImagesLoaded(true);
     }
