813302b0a49c36819ce4288e3c98263e43a755ba
Tim <timdobranski@gmail.com>
2024-01-16 20:22:59 -0800

    Added title to Photo upload inputs

--- components/Photo/Photo.js
+++ components/Photo/Photo.js
@@ -5,12 +5,16 @@ import styles from './photo.module.css';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTableCells, faTv } from '@fortawesome/free-solid-svg-icons';
+import { faTableCells, faTv, faX } from '@fortawesome/free-solid-svg-icons';
 import Image from 'next/image';
 
 export default function Photo({ updatePhotoContent, src, isEditable, updatePhotoFormat, format, setActiveBlock }) {
   const [selectedFiles, setSelectedFiles] = useState([]);
   const [photoFormat, setPhotoFormat] = useState(format); // grid, single, carousel
+  const [currentPhotoIndex, setCurrentPhotoIndex] = useState(0);
+  const handleCarouselChange = (index) => {
+    setCurrentPhotoIndex(index);
+  };
   const formatSelectionInterface = () => {
     return (
       <div className={styles.formatSelection}>
@@ -41,7 +45,7 @@ export default function Photo({ updatePhotoContent, src, isEditable, updatePhoto
           count + (fileObj.file instanceof File ? 1 : 0), 0);
 
       if (readersToComplete === 0) {
-          updatePhotoContent(selectedFiles.map(({ src, caption }) => ({ src, caption })));
+          updatePhotoContent(selectedFiles.map(({ src, caption, title }) => ({ src, caption, title })));
           return;
       }
 
@@ -49,28 +53,28 @@ export default function Photo({ updatePhotoContent, src, isEditable, updatePhoto
           if (fileObj.file instanceof File) {
               const reader = new FileReader();
               reader.onloadend = () => {
-                  urls.push({ src: reader.result, caption: fileObj.caption });
+                  urls.push({ src: reader.result, caption: fileObj.caption, title: fileObj.title });
                   if (--readersToComplete === 0) {
                       updatePhotoContent(urls);
                   }
               };
               reader.readAsDataURL(fileObj.file);
           } else {
-              urls.push({ src: fileObj.src, caption: fileObj.caption });
+              urls.push({ src: fileObj.src, caption: fileObj.caption, title: fileObj.title });
           }
       });
     }
   }, [isEditable]);
 
-
-
   // if there are existing photos, render them
   useEffect(() => {
+    console.log('src in useEffect: ', src)
     if (src && src.length > 0) {
       const fileObjects = src.map(photo => ({
         file: null, // No file object available for existing photos
         caption: photo.caption || '',
-        src: photo.src // Keep the existing URL
+        src: photo.src, // Keep the existing URL
+        title: photo.title
       }));
       setSelectedFiles(fileObjects);
     } else {
@@ -79,13 +83,21 @@ export default function Photo({ updatePhotoContent, src, isEditable, updatePhoto
   }, [src]);
 
   const handleFileChange = (event) => {
-    // Create objects for each new file with an empty caption
-    const newFileObjects = Array.from(event.target.files).map(file => ({ file, caption: '' }));
-
-    // Append new files to the existing ones
+    const newFileObjects = Array.from(event.target.files).map(file => ({
+      file,
+      caption: '',
+      title: '', // Add title property
+    }));
     setSelectedFiles(existingFiles => [...existingFiles, ...newFileObjects]);
   };
-
+  const handleRemovePhoto = (index) => {
+    setSelectedFiles(files => files.filter((_, idx) => idx !== index));
+  };
+  const handleTitleChange = (index, newTitle) => {
+    setSelectedFiles(files =>
+      files.map((fileObj, idx) => idx === index ? { ...fileObj, title: newTitle } : fileObj)
+    );
+  };
   const handleCaptionChange = (index, newCaption) => {
     setSelectedFiles(files =>
       files.map((fileObj, idx) => { return idx === index ? { ...fileObj, caption: newCaption } : fileObj})
@@ -97,11 +109,18 @@ export default function Photo({ updatePhotoContent, src, isEditable, updatePhoto
     return selectedFiles.map((fileObj, index) => {
       const imageUrl = fileObj.file ? URL.createObjectURL(fileObj.file) : fileObj.src;
       return (
+        <>
         <div key={index} className={styles.photoPreviewContainer}>
-          {/* {selectedFiles.length > 1 ? formatSelectionInterface() : null} */}
-          <img src={imageUrl} className={styles.photoPreview} alt={`Preview ${index}`} />
-          {/* <Image src={imageUrl} alt={`Preview ${index}`} fill={true}/> */}
-
+          <div className={styles.photoWrapper}>
+            <FontAwesomeIcon icon={faX} className={styles.removePhotoIcon} onClick={() => handleRemovePhoto(index)} />
+            <img src={imageUrl} className={styles.photoPreview} alt={`Preview ${index}`} />
+          </div>
+          <input
+          value={fileObj.title}
+          onChange={(e) => handleTitleChange(index, e.target.value)}
+          placeholder="Enter title"
+          className={styles.titleInput}
+        />
           <textarea
             value={fileObj.caption}
             onChange={(e) => handleCaptionChange(index, e.target.value)}
@@ -111,6 +130,7 @@ export default function Photo({ updatePhotoContent, src, isEditable, updatePhoto
             rows={4}  // Example: Set the number of rows to make it visibly larger
           />
         </div>
+        </>
       );
     });
   };
@@ -122,6 +142,7 @@ export default function Photo({ updatePhotoContent, src, isEditable, updatePhoto
           console.log('photo caption: ', photo.caption),
           <div key={index} className={styles.gridPhotoContainer}>
             <img src={photo.src} alt={`Photo ${index}`} className={styles.gridPhoto} />
+            {photo.title && <p className={styles.photoTitle}>{photo.title}</p>}
             {photo.caption && <p className={styles.photoCaption}>{photo.caption}</p>}
           </div>
         ))}
@@ -131,17 +152,28 @@ export default function Photo({ updatePhotoContent, src, isEditable, updatePhoto
   const renderPhotosCarousel = (photos) => {
     return (
       <div className={styles.carouselWrapper}>
-      <Carousel dynamicHeight={true} autoPlay={false} showThumbs={true}>
+      <Carousel
+        dynamicHeight={true}
+        autoPlay={false}
+        showThumbs={true}
+        selectedItem={currentPhotoIndex}
+        onChange={handleCarouselChange}
+      >
         {photos.map((photoObj, index) => (
           <div key={index} className={styles.carouselSlide}>
             <img src={photoObj.src} alt={`Photo ${index}`} />
-            {photoObj.caption && (
-              <p className={styles.carouselCaption}>{photoObj.caption}</p>
+            {photoObj.title && (
+              <p className={styles.carouselCaption}>{photoObj.title}</p>
             )}
           </div>
         ))}
       </Carousel>
-      </div>
+      {photos[currentPhotoIndex] && photos[currentPhotoIndex].caption && (
+        <p className={styles.currentPhotoCaption}>
+          {photos[currentPhotoIndex].caption}
+        </p>
+      )}
+    </div>
     );
   };
 
