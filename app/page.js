cf56d6b1a78be9e5c16fd92b6895909e86dac3bb
Tim <timdobranski@gmail.com>
2024-01-21 23:27:12 -0800

    Applied red tint to all photos

--- app/page.js
+++ app/page.js
@@ -6,44 +6,26 @@ import { useRouter } from 'next/navigation';
 
 export default function Home() {
   const [rows, setRows] = useState([[], [], []]);
-  const baseImagePath = "/images/intro/";
-  const hoverImagePath = "/images/introNaturalColor/";
+  const baseImagePath = "/images/introNaturalColor/"; // Only one path needed
   const rowsDirectories = ['row1', 'row2', 'row3'];
   const imagesPerRow = 10;
   const router = useRouter();
   const [loadedImages, setLoadedImages] = useState(0);
   const [allImagesLoaded, setAllImagesLoaded] = useState(false);
-  const [hoveredRowIndex, setHoveredRowIndex] = useState(null); // Track hovered row index
-  const [hoveredImageIndex, setHoveredImageIndex] = useState(null); // Track hovered image index
-  const totalImages = 60;
+  const totalImages = 30; // Adjusted total images count
 
   const preloadImages = () => {
     rowsDirectories.forEach(rowDir => {
       for (let i = 1; i <= imagesPerRow; i++) {
         const img = new Image();
         img.src = `${baseImagePath}${rowDir}/${i}.webp`;
-        const hoverImg = new Image();
-        hoverImg.src = `${hoverImagePath}${rowDir}/${i}.webp`;
+        img.onload = handleImageLoad; // Bind the load handler here
       }
     });
   };
 
   const handleImageLoad = () => {
     setLoadedImages(prev => prev + 1);
-  };
-
-  const handleMouseEnter = (rowIndex, imageIndex) => (e) => {
-    const newSrc = e.target.src.replace(baseImagePath, hoverImagePath);
-    e.target.src = newSrc;
-    setHoveredRowIndex(rowIndex);
-    setHoveredImageIndex(`${rowIndex}-${imageIndex}`); // Set the hovered image index
-  };
-
-  const handleMouseLeave = (rowIndex, imageIndex) => (e) => {
-    const originalSrc = e.target.src.replace(hoverImagePath, baseImagePath);
-    e.target.src = originalSrc;
-    setHoveredRowIndex(null);
-    setHoveredImageIndex(null);
   };
 
   useEffect(() => {
@@ -55,16 +37,14 @@ export default function Home() {
       for (let i = 1; i <= imagesPerRow; i++) {
         rowImages.push(`${baseImagePath}${rowDir}/${i}.webp`);
       }
-      return [...rowImages, ...rowImages];
+      return rowImages;
     };
 
     const allRows = rowsDirectories.map(rowDir => loadRowImages(rowDir));
     setRows(allRows);
   }, []);
 
-
   useEffect(() => {
-    console.log('loaded images: ', loadedImages)
     if (loadedImages >= totalImages) {
       setAllImagesLoaded(true);
     }
@@ -83,24 +63,22 @@ export default function Home() {
         <h1 className={styles.enterButton}>ENTER</h1>
       </div>
       <div className={`${styles.galleryWrapper} ${allImagesLoaded ? styles.fadeIn : ''}`}>
-        {rows.map((rowImages, rowIndex) => (
-          <div
-            key={rowIndex}
-            className={`${styles.imageRow} ${hoveredRowIndex === rowIndex ? styles.hoveredRow : ''}`}
-          >
-            {rowImages.map((src, index) => (
-              <img
-                key={`${rowIndex}-${index}`}
-                src={src}
-                alt={`Image in row ${rowIndex + 1}, number ${index + 1}`}
-                className={`${styles.image} ${allImagesLoaded ? "" : 'hiddenImage'} ${hoveredImageIndex === `${rowIndex}-${index}` ? styles.hoveredImage : ''}`}
-                onLoad={handleImageLoad}
-                onMouseEnter={handleMouseEnter(rowIndex, index)}
-                onMouseLeave={handleMouseLeave(rowIndex, index)}
-              />
-            ))}
-          </div>
-        ))}
+      {rows.map((rowImages, rowIndex) => (
+  <div key={rowIndex} className={styles.imageRow}>
+    {rowImages.map((src, index) => (
+      <div key={`${rowIndex}-${index}`} className={styles.imageContainerRed}>
+      <img
+        key={`${rowIndex}-${index}`}
+        src={src}
+        alt={`Image in row ${rowIndex + 1}, number ${index + 1}`}
+        className={styles.image}
+        onLoad={handleImageLoad}
+        // No changes required for onMouseEnter and onMouseLeave
+      />
+      </div>
+    ))}
+  </div>
+))}
       </div>
     </>
   );
