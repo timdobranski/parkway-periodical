f291666d28e5c4418d5ca4873f69b325ce536ba3
Tim <timdobranski@gmail.com>
2024-04-14 12:26:03 -0700

    Removed search params use from public layout

--- app/public/layout.js
+++ app/public/layout.js
@@ -4,26 +4,14 @@ import React, { useEffect, useState, Suspense } from 'react';
 import styles from './layout.module.css';
 import Intro from '../../components/Intro/Intro';
 import AnimatedShield from '../../components/AnimatedShield/AnimatedShield';
-import { useSearchParams } from 'next/navigation'
+// import { useSearchParams } from 'next/navigation'
 import Header from '../../components/Header/Header';
 
 
 export default function Home({ children }) {
-  const [posts, setPosts] = useState(null);
-  const searchParams = useSearchParams();
-  const postId = searchParams.get('postId');
-  const skipIntro = searchParams.get('skipIntro');
   const [introRunning, setIntroRunning] = useState(true);
-  // const [windowWidth, setWindowWidth] = useState(undefined);
   const [finishedLoading, setFinishedLoading] = useState(false);
 
-  useEffect(() => {
-    const skipIntroBool = Boolean(JSON.parse(skipIntro));
-    console.log('SKIPINTROBOOL: ', skipIntroBool, typeof skipIntroBool)
-    if (skipIntroBool === true) {
-      setIntroRunning(false);
-    }
-  }, [skipIntro])
 
 
 
@@ -32,9 +20,9 @@ export default function Home({ children }) {
       {/* mobile logo */}
       <div className={styles.mobileLogo}>
         {/* {finishedLoading ? */}
-          <AnimatedShield finishedLoading={finishedLoading}/>
-          {/* : */}
-          {/* <div className={styles.animationPlaceholder}
+        <AnimatedShield finishedLoading={finishedLoading}/>
+        {/* : */}
+        {/* <div className={styles.animationPlaceholder}
           >
           LOADING...
           </div> */}
@@ -57,18 +45,16 @@ export default function Home({ children }) {
 
 
   return (
-    <Suspense>
     <div>
       <Intro
         introRunning={introRunning}
         setFinishedLoading={setFinishedLoading}
       />
       { introRunning ? (welcomeModal) : null }
-      { introRunning ? null : <Header skipAnimation={skipIntro} introRunning={introRunning}/>}
+      { introRunning ? null : <Header introRunning={introRunning}/>}
       { introRunning ? null :
         children
       }
     </div>
-    </Suspense>
   );
 }
