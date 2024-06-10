2345b1c0ff67ef6e6d4409ee048c53441d5b0c05
Tim <timdobranski@gmail.com>
2024-06-10 13:20:08 -0700

    Testing production error: removed sendgrid api key

--- utils/inviteNewUser.js
+++ utils/inviteNewUser.js
@@ -1,14 +1,15 @@
 import supabaseAdmin from './supabaseAdmin';
 
 export async function inviteNewUser(email) {
-  const { data, error } = await supabaseAdmin.auth.admin.inviteUserByEmail(email, {
-    redirectTo: '/auth/invite',
-  });
+  // const { data, error } = await supabaseAdmin.auth.admin.inviteUserByEmail(email, {
+  //   redirectTo: '/auth/invite',
+  // });
 
-  if (error) {
-    console.error('Error inviting user:', error);
-    return null;
-  }
+  // if (error) {
+  //   console.error('Error inviting user:', error);
+  //   return null;
+  // }
 
-  return data;
+  // return data;
+  console.log('inside utils/inviteNewUser')
 }
--- utils/supabaseAdmin.js
+++ utils/supabaseAdmin.js
@@ -1,12 +1,12 @@
-import { createClient } from '@supabase/supabase-js';
+// import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseServiceRoleKey = process.env.NEXT_SUPABASE_SERVICE_ROLE_KEY;
-
+console.log('supabase service key: ', supabaseServiceRoleKey);
 
 // THIS SUPABASE CLIENT USES THE SERVICE ROLE KEY -- IT SHOULD NOT BE USED IN THE FRONTEND
-const supabaseAdmin = createClient(supabaseUrl, supabaseServiceRoleKey);
+// const supabaseAdmin = createClient(supabaseUrl, supabaseServiceRoleKey);
 // THIS SUPABASE CLIENT USES THE SERVICE ROLE KEY -- IT SHOULD NOT BE USED IN THE FRONTEND
 
 
-export default supabaseAdmin;
+// export default supabaseAdmin;
