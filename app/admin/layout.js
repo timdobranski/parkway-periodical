81136e6eb2eeaa2306bacdfc23cbaa9332980008
Tim <timdobranski@gmail.com>
2024-07-14 00:00:38 -0700

    Added block on admin side for mobile devices and message to user requiring laptop or desktop devices at this time

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -5,6 +5,7 @@ import { useRouter, usePathname } from 'next/navigation';
 import { createClient } from '../../utils/supabase/client'; // Update the path as per your directory structure
 import HeaderAdmin from '../../components/HeaderAdmin/HeaderAdmin';
 import { useAdmin } from '../../contexts/AdminContext';
+import styles from './layout.module.css';
 import ErrorHandling from '../../components/ErrorHandling/ErrorHandling';
 
 export default function AdminLayout({ children }) {
@@ -18,14 +19,22 @@ export default function AdminLayout({ children }) {
     return <div className='loadingMessage'>Loading...</div>
   }
 
+  const mobileSupportMessage = (
+    <div className={styles.mobileSupportMessageWrapper}>
+      <img src='/images/logos/parkway.webp' alt='Parkway Academy Logo' className={styles.logo} />
+      <p>Creating & editing content is currently not supported on mobile devices. To access the administrative side of the app, please use a laptop or desktop device.</p>
+    </div>
+  )
+
   return (
     <>
-        {pathname !== 'admin/register' && <HeaderAdmin />}
-        <div className='adminPageWrapper'>
-          <Suspense>
-            {children}
-          </Suspense>
-        </div>
+      {pathname !== 'admin/register' && <HeaderAdmin />}
+      <div className='adminPageWrapper'>
+        {mobileSupportMessage}
+        <Suspense>
+          {children}
+        </Suspense>
+      </div>
     </>
   );
 }
