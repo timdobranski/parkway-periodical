dad8e7dd0941e4359b8a3ebf0ea30b9b17ef29a4
Tim <timdobranski@gmail.com>
2024-07-18 18:27:38 -0700

    Updated date formatter to accept different formats and adjust output accordingly

--- utils/dateFormatter.js
+++ utils/dateFormatter.js
@@ -1,19 +1,21 @@
+import { parseISO, format } from 'date-fns';
+
 export default function formatDate(dateStr) {
-  // Create a date object from the input string
-  const dateObj = new Date(dateStr);
+  // Parse the input date string to a Date object
+  const dateObj = parseISO(dateStr);
 
   // Get the current year
   const currentYear = new Date().getFullYear();
 
   // Extract the year, month, and day from the date object
   const year = dateObj.getFullYear();
-  const month = dateObj.toLocaleString('default', { month: 'long' }); // 'short' gives the abbreviated month
-  const day = dateObj.getDate();
+  const month = format(dateObj, 'MMMM'); // Full month name
+  const day = dateObj.getUTCDate(); // Get the day of the month in UTC
 
   // Check if the year is the current year and format accordingly
   if (year === currentYear) {
-      return `${month} ${day.toString().padStart(2, '0')}`; // Pad the day to ensure two digits
+    return `${month} ${day}`; // No zero-padding for single-digit dates
   } else {
-      return `${month}-${day.toString().padStart(2, '0')}, ${year}`;
+    return `${month} ${day}, ${year}`;
   }
 }
