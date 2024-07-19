0ef26f096d45bf7fb60d9c085ab1f0d9daeda7bb
Tim <timdobranski@gmail.com>
2024-07-19 00:33:39 -0700

    Bug fixes: atom icon in animatedShield transitions between animations seamlessly, and admin side header is now rendered unconditionally to all authenticated users

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -15,6 +15,10 @@ export default function AdminLayout({ children }) {
   const { isLoading, setIsLoading, saving, setSaving, alerts, setAlerts, user, setUser } = useAdmin();
 
 
+  if (!user) {
+    return <div className='loadingMessage'>Loading...</div>
+  }
+
   const mobileSupportMessage = (
     <div className={styles.mobileSupportMessageWrapper}>
       <img src='/images/logos/parkway.webp' alt='Parkway Academy Logo' className={styles.logo} />
@@ -24,7 +28,7 @@ export default function AdminLayout({ children }) {
 
   return (
     <>
-      {pathname !== 'admin/register' && <HeaderAdmin />}
+      <HeaderAdmin />
       <div className='adminPageWrapper'>
         {mobileSupportMessage}
         <Suspense>
